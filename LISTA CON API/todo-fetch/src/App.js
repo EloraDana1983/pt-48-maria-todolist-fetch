749470e7649d74886ec07114770508820627b505
María García <139761822+EloraDana1983@users.noreply.github.com>
2024-02-06 11:03:21 +0000

    todo

--- LISTA CON API/todo-fetch/src/App.js
+++ LISTA CON API/todo-fetch/src/App.js
@@ -1,6 +1,4 @@
-
-import './App.css';
-import ListaDeTareas from './components/ListaDeTareas';
+import ListaDeTareas from "./components/ListaDeTareas";
 
 function App() {
   return (
@@ -8,7 +6,7 @@ function App() {
     <div className="card-list">
       
       <div className='add-task'>
-        <h1>Mis tareas</h1>
+        
         <ListaDeTareas />
         
       </div>
--- LISTA CON API/todo-fetch/src/components/ListaDeTareas.jsx
+++ LISTA CON API/todo-fetch/src/components/ListaDeTareas.jsx
@@ -1,74 +1,178 @@
-import React, { useState, useEffect } from 'react'
-import EntradaTarea from './EntradaTarea';
-import Tarea from './Tarea';
-import '../estilos/ListaDeTareas.css';
+import React, { useState, useEffect } from 'react';
+import "../App.css"
 
-const URL = 'https://playground.4geeks.com/apis/fake/todos/user/mariagarcia'
-const GET_HTTP_METHOD = 'GET';
-const PUT_HTTP_METHOD = 'PUT';
+const BASE_URL = 'https://playground.4geeks.com/apis/fake/todos/user/';
 
-let nextId = 1;
+const ListaDeTareas = () => {
+  const [todos, setTodos] = useState([]);
+  const [todo, setTodo] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [users, setUsers] = useState([]);
+  const [username, setUsername] = useState('Maria');
 
-function ListaDeTareas() {
-
-  const [listaTareas, setListaTareas] = useState([]);
-
-  const getLista = async () => {
-    const response = await fetch(URL, { method: GET_HTTP_METHOD });
-    const todoListState = await response.json();
-    setListaTareas(todoListState);
+  const getAllUsers = async () => {
+    setLoading(true);
+    const response = await fetch(`${BASE_URL}`);
+    const data = await response.json();
+    setUsers(data);
+    setLoading(false);
   };
 
-  const addTarea = async (label) => {
-    const state = [...listaTareas, { label, done: false }];
-  
-    await fetch(URL, {
-      method: PUT_HTTP_METHOD,
-      body: JSON.stringify(state),
+  const deleteUser = async (usernameToDelete) => {
+    setLoading(true);
+    try {
+      const response = await fetch(`${BASE_URL}${usernameToDelete}`, {
+        method: 'DELETE',
+      });
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      await getAllUsers();
+    } catch (error) {
+      console.error('Error deleting user:', error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const getTodos = () => {
+    setLoading(true);
+    fetch(`${BASE_URL}${username}`)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then((data) => {
+        if (Array.isArray(data)) {
+          setTodos(data);
+        } else {
+          console.error('Data is not an array:', data);
+        }
+      })
+      .catch((error) => console.error('Error fetching todos:', error))
+      .finally(() => setLoading(false));
+  };
+
+  const createUser = async () => {
+    setLoading(true);
+    const response = await fetch(`${BASE_URL}${username}`, {
+      method: 'POST',
+      body: JSON.stringify([]),
       headers: {
         'Content-Type': 'application/json',
       },
     });
-    await getLista();
+    const data = await response.json();
+    getTodos();
+    setLoading(false);
+  };
+
+  const deleteTodo = async (id) => {
+    console.log('Deleting todo with id:', id);
+    setLoading(true);
+    const updatedTodos = todos.filter((todo) => todo.id !== id);
+    console.log('Updated todos:', updatedTodos);
+    const response = await fetch(`${BASE_URL}${username}`, {
+      method: 'PUT',
+      body: JSON.stringify(updatedTodos),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    const data = await response.json();
+    setTodos(updatedTodos);
+    setLoading(false);
+  };
+
+  const addTodo = async () => {
+    setLoading(true);
+    const updatedTodos = [...todos, { label: todo, done: false }];
+    const response = await fetch(`${BASE_URL}${username}`, {
+      method: 'PUT',
+      body: JSON.stringify(updatedTodos),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    const data = await response.json();
+    setTodo('');
+    getTodos();
+    setLoading(false);
+  };
+
+  const deleteAllTodos = async () => {
+    setLoading(true);
+    try {
+      const response = await fetch(`${BASE_URL}${username}`, {
+        method: 'DELETE',
+      });
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      setTodos([]); // Vacía la lista de tareas en el estado local
+    } catch (error) {
+      console.error('Error deleting all todos:', error);
+    } finally {
+      setLoading(false);
+    }
   };
 
   useEffect(() => {
-    getLista();
+    getTodos();
   }, []);
 
-  const deleteTarea = async (id) => {
-    const actualizarTareas = listaTareas.filter((tarea) => tarea.id !== id);
-    await fetch(`${URL}`, {
-      method: 'PUT',
-      body: JSON.stringify(actualizarTareas),
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
-    setListaTareas(actualizarTareas);
-  };
-
-
-  // const deleteTask = id => {
-  //   setListaTareas(listaTareas.filter(tarea => tarea.id !== id));
-  // };
-    
   return (
     <>
-      <EntradaTarea addTarea={addTarea}/>
-
-      <div className='list-task'>
-        {
-          listaTareas.length > 0 ?
-          listaTareas.map((Task) =>
-            <Tarea key={Task.id} {...Task} deleteTarea={deleteTarea} />
-          ):
-          <h3>No hay tareas, añadir tareas</h3>
-        }
-      </div>
-
+      <div className='registro'>
+        <h4>Registra tu usuario</h4>
+        <div>
+          <input
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <button className='usuario' onClick={createUser}>Crear Usuario</button>
+        </div>
+        <button className='ver usuario' onClick={getAllUsers}>Ver usuarios</button>
+        <ul>
+          {users.map((user, index) => (
+            <li key={index}>
+              {user}
+              <button className='borrar-una' onClick={() => deleteUser(user)}>🗑️</button>
+            </li>
+          ))}
+        </ul>
+      </div> 
+      <div className='tareas'>
+        <div>
+          <input
+            value={todo}
+            onChange={(e) => setTodo(e.target.value)}
+            placeholder="Nueva tarea"
+          />
+          <button className='añadir' onClick={addTodo}>Añadir tarea</button>
+        </div>
+        <button className='ver' onClick={getTodos}>Ver tareas</button>
+        <button className='borrar-todas' onClick={deleteAllTodos}>Borrar tareas</button>
+        {loading ? (
+          <div>Loading...</div>
+        ) : (
+          Array.isArray(todos) && (
+            <ul>
+              {todos.map((todo, index) => (
+                <li key={`${todo.id}-${index}`}>
+                  {todo.label}
+                  <button onClick={() => deleteTodo(todo.id)}>Delete</button>
+                </li>
+              ))}
+            </ul>
+          )
+        )}
+      </div> 
     </>
-  )
-}
+  );
+};
 
-export default ListaDeTareas
+export default ListaDeTareas;
